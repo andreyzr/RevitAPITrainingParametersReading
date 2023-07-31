30a7ab5a3c95a439bd123bcec557835016f24dae
andreyzr43 <andreyzr43@yandex.ru>
2023-07-31 07:37:08 +0300

    3

---
+++ HW3.4AddingDiameterParamete/Main.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3._4AddingDiameterParamete
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var categorySet = new CategorySet();
+            categorySet.Insert(Category.GetCategory(doc, BuiltInCategory.OST_PipeCurves));
+
+
+            using (Transaction ts = new Transaction(doc, "Add parameter"))
+            {
+                ts.Start();
+                CreateSharedparameter(uiapp.Application, doc, "Труба", categorySet, BuiltInParameterGroup.PG_DATA, true);
+                ts.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+
+        private static void CreateSharedparameter(Application application,
+            Document doc, string parameterName, CategorySet categorySet,
+            BuiltInParameterGroup builtInParameterGroup, bool isInstance)
+        {
+            DefinitionFile definitionFile = application.OpenSharedParameterFile();
+            if (definitionFile == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден файл общих параметров");
+                return;
+            }
+
+            Definition definitions = definitionFile.Groups
+                .SelectMany(group => group.Definitions)
+                .FirstOrDefault(def => def.Name.Equals(parameterName));
+            if (definitions == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден указанный параметр");
+                return;
+            }
+
+            Binding binding = application.Create.NewTypeBinding(categorySet);
+            if (isInstance)
+            {
+                binding = application.Create.NewInstanceBinding(categorySet);
+            }
+            BindingMap map = doc.ParameterBindings;
+            map.Insert(definitions, binding, builtInParameterGroup);
+        }
+    }
+}
